a64134ae6d84352c58d70b7404950bca54e259dc
Mattia Fontana <mattia.fontana@avanade.com>
2025-12-05 20:09:44 +0100

    Day 5 - Part One

---
+++ Day5/IdRange.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class IdRange
+{
+    private UInt128 minId;
+    private UInt128 maxId;
+
+	public IdRange(string e)
+    {
+        String[] parts = e.Split('-', StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 2)
+        {
+            throw new ArgumentException("Invalid range format", nameof(e));
+        }
+        minId = UInt128.Parse(parts[0]);
+        maxId = UInt128.Parse(parts[1]);
+	}
+
+    public bool IsIdInRange(UInt128 id)
+    {
+        return id >= minId && id <= maxId;
+	}
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String path = "C:\\AdventOfCode\\Day5\\input.txt";
+
+
+if (!File.Exists(path))
+{
+    Console.WriteLine("File not found.");
+    return;
+}
+String text = File.ReadAllText(path);
+
+String[] lines = text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+var freshIdRanges = lines.Where(e => !String.IsNullOrWhiteSpace(e) && e.Contains('-')).Select(e => new IdRange(e));
+var ingredietsIds = lines.Where(e => !String.IsNullOrWhiteSpace(e) && !e.Contains('-')).Select(e => UInt128.Parse(e));
+
+var freshingredietsIds = ingredietsIds.Where(id => freshIdRanges.Any(range => range.IsIdInRange(id)));
+
+var count = freshingredietsIds.Count();
+
+Console.WriteLine("Number of fresh ingredient IDs: " + count);
+
+
+
+
+
