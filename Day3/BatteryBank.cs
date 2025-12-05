13a7a2b967c01c4169a56f11c00c8cb787462a37
Mattia Fontana <mattia.fontana@avanade.com>
2025-12-05 15:44:46 +0100

    Day3 - Part One

---
+++ Day3/BatteryBank.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class BatteryBank
+{
+    private Byte[] joltageRatings;
+
+    public BatteryBank(string line)
+    {
+        joltageRatings = line.Where(c => Char.IsDigit(c)).Select(c => byte.Parse(c.ToString())).ToArray();
+    }
+
+    public Byte findMaxJoltageCombination()
+    {
+        var sub1 = joltageRatings[..^1];
+        var max = sub1.Max();
+        var indexOfMax = Array.IndexOf(joltageRatings, max);
+        var sub2 = joltageRatings[(indexOfMax + 1)..];
+        var secondMax = sub2.Max();
+        return (Byte)((max * 10) + secondMax);
+	}
+}
---
+++ Day3/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+String path = "C:\\AdventOfCode\\Day3\\input.txt";
+
+if (!File.Exists(path))
+{
+	Console.WriteLine("File not found.");
+	return;
+}
+
+String text = File.ReadAllText(path);
+
+string[] lines = text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+IEnumerable<string> lineList = lines.Select(line => line.Trim()).Where(line => !string.IsNullOrEmpty(line));
+
+var banks = lineList.Select(line => new BatteryBank(line));
+
+IEnumerable<byte> maxCombinations = banks.Select(bank => bank.findMaxJoltageCombination());
+
+int sumOfMaxCombinations = maxCombinations.Sum(mc => mc);
+
+Console.WriteLine("Sum of max joltage combinations: " + sumOfMaxCombinations);
+
