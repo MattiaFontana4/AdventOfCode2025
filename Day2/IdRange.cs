a745144904d25d51849de896664a4bd563212720
Mattia Fontana <mattia.fontana@avanade.com>
2025-12-05 14:47:53 +0100

    Day2 - Part one

---
+++ Day2/IdRange.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class IdRange
+{
+	public UInt128 MinId { get; private set; }
+	public UInt128 MaxId { get; private set; }
+
+	public IdRange(String range)
+	{
+		String[] parts = range.Split('-', StringSplitOptions.RemoveEmptyEntries);
+		if (parts.Length != 2)
+		{
+			throw new ArgumentException("Invalid range format", nameof(range));
+		}
+
+		MinId = UInt128.Parse(parts[0]);
+		MaxId = UInt128.Parse(parts[1]);
+	}
+
+	public IEnumerable<UInt128> GetInvalidIds()
+	{
+		List<UInt128> invalid = new List<UInt128>();
+
+		for (UInt128 id = MinId; id <= MaxId; id++)
+		{
+			String idStr = id.ToString();
+
+			if (idStr.Length % 2 == 0) 
+			{
+				String idStrSub1 = idStr.Substring(0, idStr.Length / 2);
+				String idStrSub2 = idStr.Substring(idStr.Length / 2, idStr.Length / 2);
+				
+				if (idStrSub1 == idStrSub2)
+				{
+					invalid.Add(id);
+				}
+			}
+		}
+
+		return invalid;
+	}
+
+
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+String path = "C:\\AdventOfCode\\Day2\\input.txt";
+
+if (!File.Exists(path))
+{
+	Console.WriteLine("File not found.");
+	return;
+}
+
+String text = File.ReadAllText(path);
+String[] lines = text.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+IEnumerable<IdRange> ranges = lines.Where(e => !String.IsNullOrEmpty(e))
+	.Select(e => new IdRange(e));
+
+IEnumerable<UInt128> invalidIds = ranges.SelectMany(r => r.GetInvalidIds()).Distinct();
+
+UInt128 sum = invalidIds.Aggregate<UInt128, UInt128>(0, (current, id) => current + id);
+
+Console.WriteLine("sum of the invalid ids: " + sum);
+
